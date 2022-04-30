09295779197f833061950c3248dfed61b197b678
Sho Nakatani <lay.sakura@gmail.com>
2022-04-30 09:08:05 +0900

    build: editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,8 +23,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/SpringQL/SpringQL.github.io/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
